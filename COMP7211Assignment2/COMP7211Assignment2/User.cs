409ad47f61e406acb200c0cd572370c3066fff24
Hee Min Chae <30003457@student.toiohomai.ac.nz>
2020-04-30 13:53:49 +1200

    Beginning development on regular student view, starting with the courses page.

---
+++ COMP7211Assignment2/COMP7211Assignment2/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP7211Assignment2
+{
+    //Code by Min 30003457
+    class User
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int StudentID { get; set; }
+        public List<Course> EnrolledCourses { get; set; }
+        public bool IsRep { get; set; }
+
+        //All students enrolled in same courses for now...
+        public User(string fn, string ln, int studentId, bool isRep)
+        {
+            FName = fn;
+            LName = ln;
+            StudentID = studentId;
+            EnrolledCourses.Add(new Course("Routing and Switching", 6201));
+            EnrolledCourses.Add(new Course("Professional Practice", 6205));
+            EnrolledCourses.Add(new Course("Advanced GUI", 7211));
+            EnrolledCourses.Add(new Course("AI", 7212));
+            IsRep = isRep;
+        }
+    }
+}
