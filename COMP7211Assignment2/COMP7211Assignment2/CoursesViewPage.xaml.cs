343bd28a4f7fed7ab41467a321eb4acca1ff71ab
Hee Min Chae <30003457@student.toiohomai.ac.nz>
2020-05-01 14:08:29 +1200

    Course detection basic functionality complete, small bug on last Course card being flattened.

--- COMP7211Assignment2/COMP7211Assignment2/CoursesViewPage.xaml.cs
+++ COMP7211Assignment2/COMP7211Assignment2/CoursesViewPage.xaml.cs
@@ -13,24 +13,52 @@ namespace COMP7211Assignment2
     {
         PlaceholderDatabase db = new PlaceholderDatabase();
         CourseDetector cd;
+        StackLayout masterStackLayout;
         public CoursesViewPage()
         {
+            masterStackLayout = new StackLayout();
+
             InitializeComponent();
-            CreateCourseCards();
+            CreateHeaderView();
+            CreateCourseCardsView();
+            
+            Content = masterStackLayout;
         }
 
-        private void CreateCourseCards()
+        private void CreateHeaderView()
+        {
+            Grid headerGrid = new Grid();
+            headerGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            headerGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            headerGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            var midCol = new Label {
+                Text = "Courses", HorizontalTextAlignment = TextAlignment.Center,
+                Padding = new Thickness(0,20,0,0), FontSize = 24, TextColor = Color.Black};
+            var rightCol = new Button
+            {
+                TranslationY = 12,
+                TranslationX = -10,
+                WidthRequest = 100,
+                HorizontalOptions = LayoutOptions.End,
+                Text = "LOGOUT"
+            };
+            headerGrid.Children.Add(midCol, 1, 0);
+            headerGrid.Children.Add(rightCol, 2, 0);
+            masterStackLayout.Children.Add(headerGrid);
+        }
+
+        private void CreateCourseCardsView()
         {
             StackLayout stackLayout = new StackLayout();
             stackLayout.TranslationY = 20;
 
             SimulateLoginUser();
 
-            List<StackLayout> CourseCards = new List<StackLayout>();
+            List<StackLayout> courseCards = new List<StackLayout>();
 
             for (int i = 0; i < cd.ReturnCourses().Count; i++)
             {
-                CourseCards.Add(new StackLayout
+                courseCards.Add(new StackLayout
                 {
                     BackgroundColor = Color.Gray,
                     HorizontalOptions = LayoutOptions.CenterAndExpand,
@@ -43,18 +71,19 @@ namespace COMP7211Assignment2
                 grid.RowDefinitions.Add(new RowDefinition());
                 grid.Children.Add(new Image());
 
-                CourseCards[i].Children.Add(grid);
-
-                StackLayout childStackLayout = new StackLayout { Padding = 10 };
+                StackLayout childStackLayout = new StackLayout { Padding = new Thickness(10, 10, 10, 15) };
                 childStackLayout.Children.Add(new Label { Text = $"COMP{cd.ReturnCourses()[i].ID}", FontSize = 36, TextColor = Color.White });
                 childStackLayout.Children.Add(new Label { Text = cd.ReturnCourses()[i].Name, TextColor = Color.White });
-                CourseCards[i].Children.Add(childStackLayout);
-                
-                stackLayout.Children.Add(CourseCards[i]);
+                courseCards[i].Children.Add(childStackLayout);
+                grid.Children.Add(childStackLayout);
+                courseCards[i].Children.Add(grid);
+                stackLayout.Children.Add(courseCards[i]);
             }
 
             ScrollView scrollView = new ScrollView();
             scrollView.Content = stackLayout;
+            masterStackLayout.Children.Add(scrollView);
+            //Content = scrollView;
         }
 
         private void SimulateLoginUser()
