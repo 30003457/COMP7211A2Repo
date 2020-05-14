ac9473642758f336c998f746b0c8cca9ccbb8762
Hee Min Chae <30003457@student.toiohomai.ac.nz>
2020-05-14 13:03:55 +1200

    Courses now correctly detected based on user's enrolment.

--- COMP7211Assignment2/COMP7211Assignment2/User.cs
+++ COMP7211Assignment2/COMP7211Assignment2/User.cs
@@ -12,14 +12,17 @@ namespace COMP7211Assignment2
         public int StudentID { get; set; }
         public List<Course> EnrolledCourses { get; set; }
         public bool IsRep { get; set; }
+        PlaceholderCourseDatabase cdb;
 
         //All students enrolled in same courses for now...
         public User(string fn, string ln, int studentId, bool isRep)
         {
+            cdb = new PlaceholderCourseDatabase();
+
             FName = fn;
             LName = ln;
             StudentID = studentId;
-            EnrolledCourses = PlaceholderCourseDatabase.records;
+            EnrolledCourses = cdb.records;
             IsRep = isRep;
         }
     }
