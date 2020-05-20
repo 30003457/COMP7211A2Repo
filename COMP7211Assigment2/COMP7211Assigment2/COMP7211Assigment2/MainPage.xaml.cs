e9cb7141d959f1b529172fb078958d39410d83e2
Tama Whata <tamawhata01@gmail.com>
2020-05-21 10:05:32 +1200

    PostPages

---
+++ COMP7211Assigment2/COMP7211Assigment2/COMP7211Assigment2/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace COMP7211Assigment2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void HomeButton(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+        
+        private void FirstPostButton(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewPostButton(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
