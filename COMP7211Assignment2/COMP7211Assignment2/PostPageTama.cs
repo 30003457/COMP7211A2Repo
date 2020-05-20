663f57b9ebc417aa76ac29dcce8e301ed9e4f9e5
Tama Whata <tamawhata01@gmail.com>
2020-05-21 10:32:37 +1200

    MainPageFix

---
+++ COMP7211Assignment2/COMP7211Assignment2/PostPageTama.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace COMP7211Assignment2
+{
+    public partial class PostPageTama : ContentPage
+    {
+        public PostPageTama()
+        {
+            InitializeComponent();
+        }
+
+        private async void NewPostButton(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CreatePostPage());
+        }
+
+        private async void HomeButton(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void BackButton(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void FirstPostButton(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PostWithRepliesPage());
+        }
+    }
+}
