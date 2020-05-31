df5bd57d39f18104ddc46f19b422a53d244c654b
lewisevanss1 <27033957@student.toiohomai.ac.nz>
2020-05-31 15:07:32 +1200

    commit

---
+++ COMP7211Assignment2/COMP7211Assignment2/Model Folder/StudentLoginFirebaseRetriever.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Code by Lewis Evans 27033957
+namespace COMP7211Assignment2.Model_Folder
+{
+    class StudentLoginFirebaseRetriever
+    {
+    }
+}
