b565fa440f034e3fb967fcd8315cea1b33615a11
patrick crampton <pcrampton1@hotmail.com>
2020-06-08 13:18:49 +1200

    fixed vote

--- COMP7211Assignment2/COMP7211Assignment2/Model Folder/Vote.cs
+++ COMP7211Assignment2/COMP7211Assignment2/Model Folder/Vote.cs
@@ -1,6 +1,25 @@
-﻿namespace COMP7211Assignment2.Model_Folder
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP7211Assignment2.Model_Folder
+
 {
-    internal class Vote
+    class Vote
     {
+        public int AddUpVote(int UpVote)
+        {
+
+            return UpVote++;
+
+        }
+
+        public int addDwnvote(int Dwnvote)
+        {
+            return Dwnvote++;
+        }
+
+
     }
 }
