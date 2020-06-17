f1423bebcfdf5a6c1d3ec172b298880f37a82532
Tama Whata <tamawhata01@gmail.com>
2020-06-17 14:58:13 +1200

    Changed createpost labels to editor boxes and added database code to createpostbutton

--- COMP7211Assignment2/COMP7211Assignment2/View Folder/CreatePostPage.xaml.cs
+++ COMP7211Assignment2/COMP7211Assignment2/View Folder/CreatePostPage.xaml.cs
@@ -3,15 +3,29 @@ using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using COMP7211Assignment2.Controller_Folder;
 
 namespace COMP7211Assignment2
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CreatePostPage : ContentPage
     {
+        FirebaseHelper firebaseHelper = new FirebaseHelper();
+        User userDetails = new User();
         public CreatePostPage()
         {
             InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+
+            base.OnAppearing();
+            var allPersons = await firebaseHelper.GetAllPersons();
         }
 
         private async void HomeButton(object sender, EventArgs e)
@@ -21,7 +35,12 @@ namespace COMP7211Assignment2
 
         private async void CreatePostButton(object sender, EventArgs e)
         {
-            //await Navigation.PushAsync(new PostWithRepliesPage((Post)sender));
+            await firebaseHelper.AddPost(PostTitle.Text, PostContent.Text, );
+            PostTitle.Text = string.Empty;
+            PostContent.Text = string.Empty;
+            await DisplayAlert("Success", "Post Created", "OK");
+            var allPersons = await firebaseHelper.GetAllPersons();
+            
         }
     }
 }
