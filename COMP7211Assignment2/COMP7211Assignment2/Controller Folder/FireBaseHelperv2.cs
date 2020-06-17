11fa2ec20f61128f269d70bf5545cacec2320b52
Hee Min Chae <30003457@student.toiohomai.ac.nz>
2020-06-17 15:31:04 +1200

    Re-organised some code for firebase helper

--- COMP7211Assignment2/COMP7211Assignment2/Controller Folder/FireBaseHelperv2.cs
+++ COMP7211Assignment2/COMP7211Assignment2/Controller Folder/FireBaseHelperv2.cs
@@ -19,6 +19,41 @@ namespace COMP7211Assignment2
     {
         public FirebaseClient firebase = new FirebaseClient($"https://student-rep-app.firebaseio.com/");
 
+        public async Task<List<Post>> GetAllPersons()
+        {
+            return (await firebase
+              .Child("Posts")
+              .OnceSingleAsync<List<Post>>());
+            //.OnceAsync<Post>()).Select(item => new Post
+            //{
+            //    Title = item.Object.Title,
+            //    Content = item.Object.Content,
+            //    Id = item.Object.Id,
+            //    CourseId = item.Object.CourseId,
+            //    Time = item.Object.Time,
+            //    TimeString = item.Object.TimeString,
+            //    Upvotes = item.Object.Upvotes,
+            //    Downvotes = item.Object.Downvotes
+            //}).ToList();
+        }
+
+        public async Task AddPost(string content, string title, int id, int courseId, DateTime time, int upvotes, int downvotes)
+        {
+
+            await firebase
+              .Child("Posts")
+              .PutAsync(new Post(id, courseId, time, title, content) { Upvotes = upvotes, Downvotes = downvotes });
+        }
+
+
+        public async Task AddReply(string content, int id, int postId, DateTime time, string timeString, int upvotes, int downvotes)
+        {
+
+            await firebase
+              .Child("PostReply")
+              .PostAsync(new PostReply(id, postId, time, content) { Upvotes = upvotes, Downvotes = downvotes });
+        }
+
         public async void SetPassword(int studentId, string pw)
         {
             var users = await GetAllUsers();
